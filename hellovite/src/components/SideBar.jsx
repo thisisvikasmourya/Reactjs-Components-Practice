595b0704b908731f812a9952b469ee314feab1e2
thisisvikasmourya <60725905+thisisvikasmourya@users.noreply.github.com>
2024-02-16 19:00:30 +0000

    done

--- hellovite/src/components/SideBar.jsx
+++ hellovite/src/components/SideBar.jsx
@@ -4,7 +4,6 @@ import Button from './Button'
 function SideBar() {
   return (
     <div>
-
         <Button children={`Hello jsx from Sidebar`} bgColor='bg-gray-900' textColor='text-white' className='shadow-xl' />
     </div>
   )
